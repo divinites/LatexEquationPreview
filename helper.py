0698064239783819e822de92dad49d7f8a5d8b82
divinites <divinites@gmail.com>
2016-10-26 13:08:48 +0100

    first public version

--- helper.py
+++ helper.py
@@ -5,7 +5,6 @@ import base64
 from subprocess import CalledProcessError
 import os
 
-
 HEAD = lambda x: """
 \\nonstopmode
 \\documentclass[preview]{standalone}
@@ -22,6 +21,21 @@ BLOCK_SCOPE = "meta.environment.math.block.be.latex"
 
 INLINE_FLAG = 0
 BLOCK_FLAG = 1
+
+
+class Setting:
+    def __init__(self):
+        self.settings = None
+
+    def get(self, key, value=None):
+        if self.settings:
+            return self.settings.get(key, value)
+
+    def update(self, setting_object):
+        self.settings = setting_object
+
+
+settings = Setting()
 
 
 class ViewConverter:
@@ -50,7 +64,8 @@ class ViewConverter:
             if end > self.view.size():
                 return
         if selector == BLOCK_SCOPE:
-            return sublime.Region(self.view.line(start).a, self.view.line(end).b), BLOCK_FLAG
+            return sublime.Region(
+                self.view.line(start).a, self.view.line(end).b), BLOCK_FLAG
         elif selector == INLINE_SCOPE:
             return sublime.Region(start + 1, end), INLINE_FLAG
         else:
@@ -62,7 +77,9 @@ class FileConverter:
         self.directory = directory
 
     def create_temptex(self, content):
-        with tempfile.NamedTemporaryFile(suffix=".tex", prefix="eqn", dir=self.directory, delete=False) as texfile:
+        with tempfile.NamedTemporaryFile(
+                suffix=".tex", prefix="eqn", dir=self.directory,
+                delete=False) as texfile:
             texfile.write(bytes(HEAD(content), 'UTF-8'))
             return texfile.name
 
@@ -78,16 +95,27 @@ class FileConverter:
         directory, _ = os.path.split(file)
         os.chdir(directory)
         compile_cmd = [pdflatex, file]
-        convert_cmd = [convert, '-density', '300', pdf_name, "-resize", "75%", png_name]
+        convert_cmd = [convert, '-density', '300', pdf_name, "-resize", "75%",
+                       png_name]
         trim_cmd = [convert, png_name, '-trim', png_name]
-        color = sublime.load_settings("latex_equation_preview.sublime-settings").get('equation_color', 'red')
-        size = sublime.load_settings("latex_equation_preview.sublime-settings").get('equation_size', '100%')
-        color_cmd = [convert, png_name, "-alpha", "off", "-fuzz", "10%", '-fill', color, '-opaque', 'black', "-alpha", "on", png_name]
-        resize_cmd = [convert, png_name, "-resize", size, png_name]
-        command_sequence = [compile_cmd, convert_cmd, trim_cmd, color_cmd]
+        foreground_color = settings.get('equation_foreground_color', 'red')
+        # background_color = sublime.load_settings(
+        #     "latex_equation_preview.sublime-settings").get(
+        #         'equation_background_color', "none")
+        size = settings.get('equation_size', '100%')
+        foreground_color_cmd = [convert, png_name, "-alpha", "off", "-fuzz",
+                                "10%", '-fill', foreground_color, '-opaque',
+                                'black', "-alpha", "on", png_name]
+        command_sequence = [compile_cmd, convert_cmd, trim_cmd,
+                            foreground_color_cmd]
         if size != "100%":
+            resize_cmd = [convert, png_name, "-resize", size, png_name]
             command_sequence.append(resize_cmd)
-        # clean_up_cmd = [latexmk, '-C']
+        # if background_color != "none":
+        #     log("background color is {}".format(background_color))
+        #     background_color_cmd = [convert, png_name, '-background', background_color, png_name]
+        #     command_sequence.append(background_color_cmd)
+
         try:
             for cmd in command_sequence:
                 subprocess.check_call(cmd)
@@ -99,8 +127,13 @@ class FileConverter:
     def png_to_datastr(self, png_file):
         with open(png_file, "rb") as png:
             raw_data = png.read()
-        img_data = base64.b64encode(raw_data).decode()
-        return '<img style="max-width:200px" src="data:image/png;base64,{0}" />'.format(img_data)
+        return base64.b64encode(raw_data).decode()
+
+
+def log(info):
+    debug_flag = settings.get('debug', 0)
+    if debug_flag != 0:
+        print("LatexEquationPreview >>> " + info)
 
 
 class FileFormatErrorException(Exception):
@@ -109,3 +142,4 @@ class FileFormatErrorException(Exception):
 
 class LatexCommandException(Exception):
     pass
+
--- preview.py
+++ preview.py
@@ -1,23 +1,21 @@
 import sublime_plugin
 import tempfile
 import sublime
-from .helper import ViewConverter, FileConverter
+from .helper import ViewConverter, FileConverter, log, settings
 from .helper import INLINE_SCOPE, BLOCK_SCOPE, INLINE_FLAG, BLOCK_FLAG
 import threading
 
 
 def plugin_loaded():
-    global temp_dir
+    global temp_dir, settings, phantom_list, phantom_key
     temp_dir = tempfile.TemporaryDirectory(prefix='Eqn_Prev')
+    settings.update(sublime.load_settings("latex_equation_preview.sublime-settings"))
+    phantom_key = []
 
 
 def plugin_unloaded():
     global temp_dir
     temp_dir.cleanup()
-
-
-class preview_listener(sublime_plugin.ViewEventListener):
-    pass
 
 
 class ShowOutstandingPreviewCommand(sublime_plugin.WindowCommand):
@@ -32,24 +30,56 @@ class ShowOutstandingPreviewCommand(sublime_plugin.WindowCommand):
         show_equation.start()
 
 
+class CleanEquationPhantoms(sublime_plugin.WindowCommand):
+    def is_enable(self):
+        view = self.window.active_view()
+        if view.scope_name(0).startswith('text.tex'):
+            return True
+        return False
+
+    def run(self):
+        global phantom_key
+        view = self.window.active_view()
+        for key in phantom_key:
+            view.erase_phantoms(key)
+
+
 class ShowEquationPhantom(threading.Thread):
     def __init__(self, view):
         super(ShowEquationPhantom, self).__init__(self)
         self.view = view
+        self.phantom_set = sublime.PhantomSet(self.view, "latex_equation")
 
     def run(self):
-        global temp_dir
+        global temp_dir, settings, phantom_key
         view_converter = ViewConverter(self.view)
         content_region, FLAG = view_converter.find_equation_range()
+        log("region is {} and flag is {}".format(repr(content_region), repr(FLAG)))
         if content_region:
             file_converter = FileConverter(temp_dir.name)
+            log("temp_dir is {}".format(temp_dir.name))
             tex_file = file_converter.create_temptex(self.view.substr(content_region))
             png_file = file_converter.tex_to_png(tex_file)
+            log("png file is {}".format(png_file))
             png_str = file_converter.png_to_datastr(png_file)
-            self.view.add_phantom("latex_equation",
-                                  sublime.Region(content_region.b, content_region.b + 1) if FLAG == BLOCK_FLAG
-                                  else self.view.sel()[0],
-                                  png_str,
-                                  sublime.LAYOUT_BLOCK if FLAG == BLOCK_FLAG else sublime.LAYOUT_BELOW,
-                                  on_navigate=lambda href: self.view.erase_phantoms("latex_equation"))
+            html_str = """
+            <style>
+            .block {{
+                background-color:{};
+                position:relative;
+                top: -12px;
+                display: inline;
+            }}
+            </style>
+            <span class="block">
+            <a href="#"><img src="data:image/png;base64,{}" /></a>
+            </span>
+            """.format(settings.get("equation_background_color"), png_str)
+            phantom_name = "latex_equation" + str(content_region.b)
+            phantom_key.append(phantom_name)
+            self.view.add_phantom(phantom_name,
+                                  sublime.Region(content_region.b, content_region.b + 1),
+                                  html_str,
+                                  sublime.LAYOUT_BLOCK if FLAG == BLOCK_FLAG else sublime.LAYOUT_INLINE,
+                                  on_navigate=lambda href: self.view.erase_phantoms(phantom_name))
 
